ef980b59143c4aedb27398dc563d7dcfd6204092
zmtheus <matheus.nsantos@bandtec.com.br>
2022-04-26 08:00:40 -0300

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-
-    </div>
+    <footer>
+        <div class="container">
+            <img class="img--footer" src="../assets/scss/_imgs/Logo.png" alt="Logo Quatro Patas">
+            <div class="infos">
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="baseboard">
+            <p class="">Copyright 2022. Todos os direitos reservados ©.</p>
+            <p class="complaint">Violência contra animais é crime. Denuncie 0800 600 6428</p>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -11,5 +22,47 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+footer {
+    display: flex;
+    height: 35vh;
+    width: 100%;
+    background-color: $secondary-color--four;
+    flex-direction: column;
 
+    .container {
+        height: 80%;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+        .img--footer {
+            height: 100px;
+        }
+
+        h4 {
+            font-size: 18px;
+        }
+
+        p {
+            font-size: 18px;
+        }
+    .baseboard {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        height: 20%;
+        width: 100%;
+        background-color: $secondary-color--one;
+
+        p {
+            color: #FFF;
+            font-size: 20px;
+        }
+
+        .complaint {
+            font-weight: bold;
+        }
+    }
+}
 </style>
--- src/views/Adopt.vue
+++ src/views/Adopt.vue
@@ -1,9 +1,14 @@
 <template>
-    <h1>CAT & Dog</h1>
+    <footer/>
+    <footer/>
+    <Footer/>
 </template>
 
 <script>
+import Footer from '../components/Footer.vue'
+
 export default {
+    components: { Footer },
     name: 'Adopt'
 }
 </script>
