71016702ad17ae8b14cea19b531d6c33e51af07b
zmtheus <matheus.nsantos@bandtec.com.br>
2022-05-04 11:08:58 -0300

    coração funcionando

--- src/components/Fav.vue
+++ src/components/Fav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="heart--fav">
-        <div class="heart">
+        <div :class="{'heart' : favorites == true, 'full' : favorites == false}" @click="favorites = !favorites">
 
         </div>
     </div>
@@ -9,6 +9,11 @@
 <script>
 export default {
     name: 'Fav',
+    data() {
+        return {
+            favorites: true,
+        }
+    }
 }
 </script>
 
@@ -33,91 +38,86 @@ export default {
 
         /*HEART*/
         border-radius: 15px;
-        //transition: .5s ease;
         transform-origin: right bottom;
         position: relative;
         animation: heart 3s ease infinite alternate;
-    }        
-    @keyframes heart {
-        0% {
-            transform: rotate(0deg);
-        }
-        50% {
-            transform: rotate(45deg);
-            border-top-left-radius: 50px;
-            border-top-right-radius: 50px;
-        }
-        55% {
-            transform: rotate(45deg) scale(1);
-        }
-        60% {
-            transform: rotate(45deg) scale(1.1);
-        }
-        65% {
-            transform: rotate(45deg) scale(1);
-        }
-        70% {
-            transform: rotate(45deg) scale(1.1);
-        }
-        75% {
-            transform: rotate(45deg) scale(1);
-        }
-        80% {
-            transform: rotate(45deg) scale(1.1);
-        }
-        85% {
-            transform: rotate(45deg) scale(1);
-        }
-        90% {
-            transform: rotate(45deg) scale(1.1);
-        }
-        95% {
-            transform: rotate(45deg) scale(1);
-        }
-        100% {
-            transform: rotate(45deg) scale(1.1);
-            border-top-left-radius: 50px;
-            border-top-right-radius: 50px;
-        }
-    }
-    // .heart:hover {
-    //     transform: rotate(45deg);
-    //     transition: .5s ease;
-    //     border-top-left-radius: 50px;
-    //     border-top-right-radius: 50px;
-    // }
-    .heart::after{
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        border-radius: 15px;
-        background-color: $color-heart--empty;
-        //transition: .5s ease;
-        transform-origin: left bottom;
-        animation: heart--after 3s ease infinite alternate;
-    }
-    @keyframes heart--after {
-        0% {
-            transform: rotate(0deg);
-        }
-        50% {
-            transform: rotate(-90deg) translateY(40px);
-            border-top-left-radius: 50px;
-            border-top-right-radius: 50px;
-        }
-        100% {
-            transform: rotate(-90deg) translateY(40px);
-            border-top-left-radius: 50px;
-            border-top-right-radius: 50px;
-        }
-    }
 
-    // .heart:hover:after{
-    //     transform: rotate(-90deg) translateY(40px);
-    //     transition: .5s ease;
-    //     border-top-left-radius: 50px;
-    //     border-top-right-radius: 50px;
-    // }
+        &::after{
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border-radius: 15px;
+            background-color: $color-heart--empty;
+            transform-origin: left bottom;
+            animation: heart--after 3s ease infinite alternate;
+        }
+    }        
+    .full {
+        @extend .heart;
+        background-color: $color-heart--full;
+
+        &::after{
+            content: '';
+            background-color: $color-heart--full;
+        }
+    }        
+}
+@keyframes heart {
+    0% {
+        transform: rotate(0deg);
+    }
+    50% {
+        transform: rotate(45deg);
+        border-top-left-radius: 50px;
+        border-top-right-radius: 50px;
+    }
+    55% {
+        transform: rotate(45deg) scale(1);
+    }
+    60% {
+        transform: rotate(45deg) scale(1.1);
+    }
+    65% {
+        transform: rotate(45deg) scale(1);
+    }
+    70% {
+        transform: rotate(45deg) scale(1.1);
+    }
+    75% {
+        transform: rotate(45deg) scale(1);
+    }
+    80% {
+        transform: rotate(45deg) scale(1.1);
+    }
+    85% {
+        transform: rotate(45deg) scale(1);
+    }
+    90% {
+        transform: rotate(45deg) scale(1.1);
+    }
+    95% {
+        transform: rotate(45deg) scale(1);
+    }
+    100% {
+        transform: rotate(45deg) scale(1.1);
+        border-top-left-radius: 50px;
+        border-top-right-radius: 50px;
+    }
+}
+@keyframes heart--after {
+    0% {
+        transform: rotate(0deg);
+    }
+    50% {
+        transform: rotate(-90deg) translateY(40px);
+        border-top-left-radius: 50px;
+        border-top-right-radius: 50px;
+    }
+    100% {
+        transform: rotate(-90deg) translateY(40px);
+        border-top-left-radius: 50px;
+        border-top-right-radius: 50px;
+    }
 }
 </style>
